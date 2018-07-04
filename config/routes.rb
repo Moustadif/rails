e4931f24c36cea10ddaf39f47f2298687c1166ba
raafa16 <raafa16@hotmail.com>
2018-07-04 16:16:16 +0600

    display products under each category

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'product/show'
+  get 'category/show'
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'home/index'
   devise_for :users
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :categories, only: :show do
+    resources :products, only: :show
+  end
 end
