0ad7a7d929c82e2cac77404edd8395f368911e15
raafa16 <raafa16@hotmail.com>
2018-07-04 20:27:43 +0600

    removed sidebar partial. moved product categories to navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
-  def index
+  before_action :set_featured_products
+
+  def index; end
+
+  private
+
+  def set_featured_products
+    @products = Product.all.sample(20)
   end
 end
