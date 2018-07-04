d9459c364118b39953d6348fa666ab1cfe634b5e
raafa16 <raafa16@hotmail.com>
2018-07-05 02:55:41 +0600

    basic search bar to search product by name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,19 @@
 class ApplicationController < ActionController::Base
 
-  before_action :set_categories, :set_featured_products, :set_cart
+  before_action :set_categories, :set_cart
 
 
   def set_categories
     @categories = Category.all
   end
 
-  def set_featured_products
-    @products = Product.all.sample(20)
-  end
 
   def set_cart
     if user_signed_in?
       if  current_user.cart.present?
         @current_cart = current_user.cart
       else
-        @current_cart = Cart.create(user_id: current_user.id)
+        @current_cart = Cart.create!(user_id: current_user.id)
       end
     else
       @current_cart = nil
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,5 @@
 class HomeController < ApplicationController
-  before_action :set_featured_products
-
-  def index; end
-
-  private
-
-  def set_featured_products
-    @products = Product.all.sample(20)
+  def index
+    @products = Product.search(params[:name])
   end
 end
